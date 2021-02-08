d3377f7ed78c375fb1421efcec7a551311567646
JeffDavis17 <47422520+JeffDavis17@users.noreply.github.com>
2021-02-08 16:41:39 -0500

    numbers
    read in and convert each sat to double, now just find way to store

---
+++ read_nav.m
@@ -0,0 +1,13 @@
+%% Function: read_nav.m
+function [header, data] = read_nav(input)
+    file = fopen(input);
+    
+
+end
+% This is a function that reads in the Navigation file and returns
+% respective Variables
+
+
+
+
+
