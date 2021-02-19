8b1829ccd85e360f0216432bd5fc788b61666f7a
JeffDavis17 <47422520+JeffDavis17@users.noreply.github.com>
2021-02-18 21:40:26 -0500

    kinda works kinda frigged tho
    finds coordinates sometimes

--- coordinate.m
+++ coordinate.m
@@ -1,7 +1,11 @@
 %% This is a Function that determines the ECEF Coordinate position of a satellite
 
 
-function[x, y, z] = coordinate(A, e, t, toe, deltan, M0, w, cus, cuc, crs, crc, cis, cic, i0, IDOT, omega0, omega) %earth-fixed coordinates
+function[x, y, z] = coordinate(observ) %earth-fixed coordinates
+
+% Variables
+observ = num2cell(observ);
+[A, e, t, toe, deltan, M0, w, cus, cuc, crs, crc, cis, cic, i0, IDOT, omega0, omega] = deal(observ{:});
 
 
 % WGS 84 Parameters
@@ -9,9 +13,8 @@ u = 3.986005e14; %WGS84 value of earth's universal gravitational parameter (m^3/
 omegae = 7.2921151467e-5; %WGS84 value of earth's rotation rate 
 
 
-%e is eccentricity
-A = A.^2; % Semi-Major Axis
-n0 = sqrt(u/A.^3); %computed mean motion rad/sec
+A = A^2; % Semi-Major Axis
+n0 = sqrt(u/A^3); %computed mean motion rad/sec
 tk = t - toe; %time from emphemeris reference epoch
 n = n0 + deltan; %computed mean motion
 
@@ -27,7 +30,7 @@ while c < 10e-10 %iteration equation
     c = abs(Ek-E0);
 end
 
-vk = atan2(sqrt(1-e.^2)*sin(Ek)/(cos(Ek)-e)); %True anomaly
+vk = atan2(sqrt(1-e^2)*sin(Ek)/(1 - e*cos(Ek)),(cos(Ek-e))/(1 - e*cos(Ek))); %True anomaly
 phik = vk + w; %argument of latitude
 
 
@@ -40,7 +43,7 @@ deltaik = cis*sin(2*phik) + cic*cos(2*phik); %inclination correction
 
 uk = phik + deltauk; %corrected argument of latitude
 rk = A*(1-e*cos(Ek)+deltark); %corrected radius
-ik = i0 + deltaik + IDOT*yk; %inclination correction
+ik = i0 + deltaik + IDOT*tk; %inclination correction
 
 % Positions in Orbital Plane
 xkprime = rk*cos(uk);
